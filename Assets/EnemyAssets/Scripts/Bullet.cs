326c95c0393cf1fc13e6ca480c73f6a89d0ac0fb
Rhqo <rhqo123@gmail.com>
2024-05-22 21:26:49 +0900

    add hit system (Enemy)

--- Assets/EnemyAssets/Scripts/Bullet.cs
+++ Assets/EnemyAssets/Scripts/Bullet.cs
@@ -10,18 +10,34 @@ public class Bullet : MonoBehaviour
 
     void Awake()
     {
-        bulletCollider = GetComponent<Collider>();
-        bulletCollider.enabled = false;
-        Invoke("EnableCollider", 0.5f);
+        //bulletCollider = GetComponent<Collider>();
+        //bulletCollider.enabled = false;
+        //Invoke("EnableCollider", 0.1f);
     }
     void EnableCollider()
     {
-        bulletCollider.enabled = true;
+        //bulletCollider.enabled = true;
     }
     void Start()
     {
         BulletRigidbody = GetComponent<Rigidbody>();
         Destroy(gameObject, 3f);
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        EnemyAI target = other.GetComponent<EnemyAI>();
+        //PlayerController player = other.GetComponent<PlayerController>();
+
+        if (gameObject.tag == "EnemyProjectile" && other.tag == "player")
+        {
+            //player.setDamageState();
+        }
+
+        if (gameObject.tag == "PlayerProjectile" && (other.tag == "Ghost" || other.tag == "Golem" || other.tag == "Mummy"))
+        {
+            target.setDamageState();
+        }
     }
 
     // Update is called once per frame
