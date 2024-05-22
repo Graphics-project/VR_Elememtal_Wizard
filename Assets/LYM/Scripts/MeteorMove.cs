ef7ea0d98015ccc5260d1898c99834c6a0ba04e5
Renem24 <106603073+Renem24@users.noreply.github.com>
2024-05-22 19:29:11 +0900

    add: complete vfx meteor prefab

--- Assets/LYM/Scripts/MeteorMove.cs
+++ Assets/LYM/Scripts/MeteorMove.cs
@@ -6,7 +6,8 @@ public class MeteorMove : MonoBehaviour
 {
     public float speed;
     public GameObject impactPrefab;
-
+    public List<GameObject> trails;
+    
     private Rigidbody rb;
 
     // Start is called before the first frame update
@@ -36,6 +37,22 @@ public class MeteorMove : MonoBehaviour
         {
             var impactVFX = Instantiate(impactPrefab, pos, rot) as GameObject;
             Destroy(impactVFX, 5);
+        }
+
+
+        if(trails.Count > 0)
+        {
+            for(int i = 0; i< trails.Count; i++)
+            {
+                trails[i].transform.parent = null;
+                var ps = trails[i].GetComponent<ParticleSystem>();
+                if(ps != null)
+                {
+                    ps.Stop();
+                    Destroy(ps.gameObject, ps.main.duration + ps.main.startLifetime.constantMax);
+                }
+
+            }
         }
 
         Destroy(gameObject);
