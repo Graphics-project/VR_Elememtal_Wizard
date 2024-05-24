d65cde41e6118eb7683074f158ab6e3f4453ffbb
Rhqo <rhqo123@gmail.com>
2024-05-24 18:36:09 +0900

    update details hit system

--- Assets/EnemyAssets/Scripts/Bullet.cs
+++ Assets/EnemyAssets/Scripts/Bullet.cs
@@ -7,6 +7,8 @@ public class Bullet : MonoBehaviour
     private Rigidbody BulletRigidbody;
     private Collider bulletCollider;
 
+    public int damage;
+
     void Start()
     {
         BulletRigidbody = GetComponent<Rigidbody>();
@@ -25,7 +27,13 @@ public class Bullet : MonoBehaviour
 
         if (gameObject.tag == "PlayerProjectile" && (other.tag == "Ghost" || other.tag == "Golem" || other.tag == "Mummy"))
         {
-            target.setDamageState();
+            target.setDamageState(damage);
         }
+        else if (gameObject.tag == "SlowDownProjectile" && (other.tag == "Ghost" || other.tag == "Golem" || other.tag == "Mummy"))
+        {
+            target.setDamageState(damage);
+            target.SlowDown();
+        }
+
     }
 }
