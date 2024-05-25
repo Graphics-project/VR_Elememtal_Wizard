f9cb52e368152d89171182aa9b7536878048377d
Byungha Ko <120775773+Rhqo@users.noreply.github.com>
2024-05-25 15:08:40 +0900

    update GameManager
    count kills, time
    set level

--- Assets/GameManagement/GameManager.cs
+++ Assets/GameManagement/GameManager.cs
@@ -14,15 +14,22 @@ public class GameManager : MonoBehaviour
     public GameObject player;
     public Spawner spawner;
     public PoolManager pool;
+    //public UIManager uiManager;
 
     public int level = 0;   // todo : change to private
     public int currentEnemyNum = 0;
+    public int kills = 0;
+
+    private float startTime;
+    private bool isGameActive;
+    public float elapsedTime;
 
     private void Awake()
     {
         if (instance == null)
         {
             instance = this;
+            StartGame();
             DontDestroyOnLoad(gameObject);
         }
         else
@@ -30,7 +37,33 @@ public class GameManager : MonoBehaviour
             Destroy(gameObject);
         }
     }
-    
+
+    private void Update()
+    {
+        if (isGameActive)
+        {
+            elapsedTime = Time.time - startTime;
+        }
+    }
+
+    public void StartGame()
+    {
+        startTime = Time.time;
+        isGameActive = true;
+    }
+
+    public void GameOver()
+    {
+        isGameActive = false;
+        //uiManager.ShowGameOverScreen(elapsedTime);
+        //SceneManager.LoadScene("1 Start Scene");
+    }
+
+    public void setLevel(int level)
+    {
+        this.level = level;
+    }
+
     public int getCurrentEnemyNum()
     {
         return currentEnemyNum;
@@ -42,5 +75,18 @@ public class GameManager : MonoBehaviour
     public void decCurrentEnemyNum()
     {
         currentEnemyNum--;
+        kills++;
+    }
+
+    public void PauseGame()
+    {
+        Time.timeScale = 0;
+        //uiManager.ShowPauseMenu();
+    }
+
+    public void ResumeGame()
+    {
+        Time.timeScale = 1;
+        //uiManager.HidePauseMenu();
     }
 }
--- Assets/GameManagement/Portal.cs
+++ Assets/GameManagement/Portal.cs
@@ -5,13 +5,14 @@ using UnityEngine;
 public class Portal : MonoBehaviour
 {
     public Transform targetLocation;
+    public int levelToLoad;
 
     private void OnTriggerEnter(Collider other)
     {
         if (other.tag == "Player")
         {
             other.transform.position = targetLocation.position;
-           
+            GameManager.instance.setLevel(levelToLoad);
         }
     }
 }
